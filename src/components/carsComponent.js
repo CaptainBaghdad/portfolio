f6e1e3a51171a86fa0200f5685cc97f17fb56a72
me <juniorjr083@gmail.com>
2019-06-23 20:32:56 -0400

    added new carsComponent

---
+++ src/components/carsComponent.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+
+class CarsComponent extends React.Component{
+    render(){
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+
+export default CarsComponent;
